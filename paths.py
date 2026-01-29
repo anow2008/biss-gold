32ce0c768423eaef5b576b38090f1fc762167db8
anow2008 <92413624+anow2008@users.noreply.github.com>
2026-01-29 18:50:19 +0200

    Create paths.py

---
+++ paths.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+PLUGIN_NAME = "BissPro"
+PLUGIN_VERSION = "1.1"
+PLUGIN_BUILD = "2026-01-27"
+
+PLUGIN_CHANGELOG = [
+    "- Added full Settings Menu",
+    "- Added Smart/Full restart options",
+    "- Added cache control",
+    "- Added backup management",
+    "- Added auto-add advanced options",
+    "- Added language & UI settings",
+]
+
+PLUGIN_PATH = "/usr/lib/enigma2/python/Plugins/Extensions/BissPro"
+ICON_PATH = PLUGIN_PATH + "/icons/"
+
+UPDATE_URL = "https://raw.githubusercontent.com/anow2008/softcam.key/main/softcam.key"
+BISS_TXT_URL = "https://raw.githubusercontent.com/anow2008/softcam.key/refs/heads/main/biss.txt"
+
+TMP_BISS = "/tmp/biss.txt"
